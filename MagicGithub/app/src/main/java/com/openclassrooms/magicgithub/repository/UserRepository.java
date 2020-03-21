25c5df8b35523640d3661397d2dec3b399c6340d
M1thieu <mathieupierreadrien@gmail.com>
2020-03-21 15:36:59 +0100

    Oubli de code de ma part, petite correction.

--- MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -14,15 +14,17 @@ public class UserRepository {
     }
 
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        // TODO: DO
+                return apiService.getUsers();
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
+        // TODO: DO
+            apiService.generateRandomUser();
     }
 
     public void deleteUser(User user) {
-        // TODO: A modifier
+        // TODO: DO
+         apiService.deleteUser(user);
     }
 }
